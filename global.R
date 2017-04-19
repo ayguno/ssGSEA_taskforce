ce48a783da25ce8fefa1180dbac06a97c5c2ac3d
Ozan Aygun <ozan@broadinstitute.org>
2017-04-19 14:05:19 -0400

    generate GSEA plot 2

--- global.R
+++ global.R
@@ -41,10 +41,30 @@ genesets <- strsplit(readLines(gmt),"\t") # read as a list and access features
 warning("No or multiple gene sets.") 
 
 
-generate.GSEAplot <- function(feature.exp,feature.geneset){
+####################
+# Dev. purpose only
+####################
+feature.index <- 1 # Only one value, selected feature
+gene.set.index <- 1:3 # Can be multiple values, selected genesets
+
+feature.exp <- input.gct[,feature.index]; names(feature.exp) <- row.names(input.gct)
+feature.geneset <- data.frame(gset = row.names(results.gct)[gene.set.index],
+                              NES = results.gct[gene.set.index,feature.index],
+                              P.value = p.values.gct[gene.set.index,feature.index],
+                              FDR = fdr.gct[gene.set.index,feature.index]
+                              )
+feature.name <- names(input.gct)[1]
+
+generate.GSEAplot <- function(feature.name,feature.exp,feature.geneset){
 # Generates GSEAplot for a given feature (treatment, condition)
+# feature.name: name of the user-selected feature        
 # feature.exp: gene-named vector of expression values for a given feature
 # feature.geneset: data.frame contains the selected gene sets along with their NES, FDR and P-val
-        
+
+# For the base plot
+feature.exp <- feature.exp[!is.na(feature.exp)] # remove missing values
+feature.exp <- feature.exp[order(feature.exp,decreasing = TRUE)]
+
+x <- 1:length(feature.exp)
         
 }
